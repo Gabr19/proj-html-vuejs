35d0098949612aba09db0e0ac19632997fc3e2a8
Gabr19 <sannagabriele19@gmail.com>
2023-02-13 19:40:38 +0100

    inizializzazione

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script >
+export default {
+    name: 'AppHeader'
+}; 
+</script>
+
+<template>
+  <div class="container">
+    <div class="container-center">
+
+    </div>
+  </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script >
+
+export default {
+    name: 'AppMain'
+}
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>main</h1>
+
+  </div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
