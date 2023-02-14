5ef21bdfe6d0ffb728ced3b4cc9029c1371c6c21
Gabr19 <sannagabriele19@gmail.com>
2023-02-14 20:54:16 +0100

    fine header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,21 +5,69 @@ export default {
 </script>
 
 <template>
-  <div class="container">
+  <div class="container-header">
     <div class="top-header">
-      <div class="container-center">
+      <div class="container-center-top-header">
         <div class="left">
+          <div>
+            (icona) 6767 Santa Monica Blvd, Los Angeles, CA 90038
+          </div>
+          <div id="home-logo">
+            <img src="../images/mt-2236-home-logo.png" alt="">
+          </div>
         </div>
+
         <div class="right">
+          <div id="top-right">
+            <div>(icona)Call Now 123-456-7890</div>
+            <div>
+              <button class="button-white">Donate</button>
 
+            </div>
+          </div>
+          <div id="bottom-right">
+            <div>
+              <ul>
+                <li class="yellow">Home</li>
+                <li>Services</li>
+                <li>Courses</li>
+                <li>About</li>
+                <li>FAQ</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+          </div>
         </div>
-
-
       </div>
     </div>
-    <div class=" bottom-header">
+
+    <div class="bottom-header">
       <div class="container-center">
         <div class="only-left">
+          <div>
+            <button class="button-yellow-mini"> WELCOME</button>
+          </div>
+          <div>
+            <h1>
+              ACADEMIC CHESS FOR <span class="yellow italic">EVERYONE</span> 
+            </h1>
+          </div>
+          <div>
+            <h4>
+              We are dedicated to broadening and developing chess as art,
+              recreation, and as a significant element of culture in Northern California.
+            </h4>
+          </div>
+          <div class="bottom-header-button">
+            <button class="button-yellow">
+              Enroll Now
+            </button>
+
+            <button class="button-no-color">
+              Learn More
+            </button>
+          </div>
           
         </div>
         
@@ -31,6 +79,13 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+
+  .container-header{
+    background-image: url(../images/mt-2236-home-bg1.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
+  
 
 
 </style>
