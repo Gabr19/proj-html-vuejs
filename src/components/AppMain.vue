f29b1e5ea2b42ac166fe86c0472ec4db5871316c
Gabr19 <sannagabriele19@gmail.com>
2023-02-15 14:18:57 +0100

    our service section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,15 +11,64 @@ export default {
 <div class="container-our-services">
     <div class="top-our-services">
       <div class="container-center">
+        <div>
+          <h2>OUR SERVICES</h2> 
+          <p>We make your child happy day after day</p>
+        </div>
         
       </div>  
     </div>
     <div class="bottom-our-services">
       <div class="container-center">
         <div class="left">
-
+          <h3>Chess Education</h3>
+          Popular education for your child and you
+          Teachers and parents around the world know that chess is not a simple game. It is a great learning tool that will help build your child's executive function skills.
+          <div id="OS-left-list">
+            <div>
+              <img src="../images/mt-2236-home-icon-bullet.png" alt="">
+              Build self-regulation and motivation
+            </div>
+            <div>
+              <img src="../images/mt-2236-home-icon-bullet.png" alt="">
+              Help with concentration issues
+            </div>
+            <div>
+              <img src="../images/mt-2236-home-icon-bullet.png" alt="">
+              Learn to plan, organize, and prioritize
+            </div>
+            <div>
+              <img src="../images/mt-2236-home-icon-bullet.png" alt="">
+              Regulate emotions
+            </div>
+            <div>
+              <img src="../images/mt-2236-home-icon-bullet.png" alt="">
+              Learn to pay attention to detail
+            </div>
+          </div>
+          <button class="button-yellow">Learn More</button>
         </div>
         <div class="right">
+          <div>
+            <img src="../images/mt-2236-home-icon1.png" alt="">
+            <h4>Various Age Group</h4>
+            We provide chess classes to both children from 5 years old and adults.
+          </div>
+          <div>
+            <img src="../images/mt-2236-home-icon2.png" alt="">
+            <h4>Top Chess Variants</h4>
+            We don't only teach classical chess but also other top variants of this game.
+          </div>
+          <div>
+            <img src="../images/mt-2236-home-icon3.png" alt="">
+            <h4>Best Chess Players</h4>
+            Our alumni have won many prestigious chess tournaments in the country.
+          </div>
+          <div>
+            <img src="../images/mt-2236-home-icon4.png" alt="">
+            <h4>Best Chess Opening</h4>
+            Our students know all the best chess openings and how to use them.
+          </div>
 
         </div>
         
